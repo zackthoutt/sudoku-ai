1c5d615578c8ce6beb6ba2089c6c1bfbebd8a315
Zack Thoutt <zackarey.thoutt@colorado.edu>
2017-12-16 14:12:02 -0700

    refactor: initialize sudoku puzzle with helpful attributes

--- solver.py
+++ solver.py
@@ -1,22 +1,4 @@
-
-from sudoku import *
-
-def cross(A, B):
-    """Cross product of elements in A and elements in B """
-    return [x+y for x in A for y in B]
-
-
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-diagonal_units = [
-    [rows[position] + cols[position] for position in range(0, len(rows))],
-    [rows[position] + cols[-(position + 1)] for position in range(0, len(rows))]
-]
-unitlist = row_units + column_units + square_units + diagonal_units
-
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+from sudoku import Sudoku
 
 
 def naked_twins(values):
--- sudoku.py
+++ sudoku.py
@@ -3,12 +3,34 @@ class Sudoku():
     rows = 'ABCDEFGHI'
     cols = '123456789'
 
-    def __init__(self, initial_grid):
-        self.boxes = [r + c for r in self.rows for c in self.cols]
-
+    def __init__(self, initial_grid, game_type='standard'):
+        """ Initialize the Sudoku puzzle
+            Args:
+                - initial_grid (str): string representing the sudoku grid
+                  Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+                - game_type (str): the type of game to define rules
+                    Choices:
+                        1. 'standard': use the normal rules of 1-9 appearing exactly once in each row, col, and square
+                        2. 'diagonal': use the 'standard' rules above and also require 1-9 to appear exactly once in each diagonal
+        """
+        self.build_puzzle()
         self.initial_grid = initial_grid
         self.grid = initial_grid
         self.values = self.grid_to_values(initial_grid)
+
+    def build_puzzle(self):
+        self.boxes = [r + c for r in self.rows for c in self.cols]
+        self.row_units = [self.cross(r, self.cols) for r in self.rows]
+        self.column_units = [self.cross(self.rows, c) for c in self.cols]
+        self.square_units = [self.cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+        self.diagonal_units = [
+            [self.rows[position] + self.cols[position] for position in range(0, len(self.rows))],
+            [self.rows[position] + self.cols[-(position + 1)] for position in range(0, len(self.rows))]
+        ]
+        self.unitlist = self.row_units + self.column_units + self.square_units + self.diagonal_units
+
+        self.units = dict((s, [u for u in self.unitlist if s in u]) for s in self.boxes)
+        self.peers = dict((s, set(sum(self.units[s],[]))-set([s])) for s in self.boxes)
 
     def values_to_grid(self, values=None):
         """ Convert the dictionary board representation to as string
@@ -73,6 +95,11 @@ class Sudoku():
                           for c in self.cols))
             if r in 'CF': print(line)
         print()
+
+    @staticmethod
+    def cross(A, B):
+        """Cross product of elements in A and elements in B """
+        return [x+y for x in A for y in B]
 
 
 s = Sudoku('2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3')
